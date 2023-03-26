b269c8c5470282851685835113b77bcbe8dc1768
muskan2532150 <rg2532150@gmail.com>
2023-03-26 19:23:41 +0530

    Add delete link

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -7,6 +7,7 @@ const Card = () => {
                 <h5 className="card-title text-body-secondary">Card title</h5>
                 <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                 <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Delete</a>
             </div>
         </div>
     )
