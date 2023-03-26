a15a57262482dddab661ea9d192f67d679dfe1ca
muskan2532150 <rg2532150@gmail.com>
2023-03-26 20:15:52 +0530

    Add Routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar';
 import Card from './Component/Card';
+import NewCard from './Component/NewCard';
+import { store } from './store/store'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <Navbar/>
-      <Card/>
-      </header>
-    </div>
+    <Provider store={store}>
+     <Router>
+     <Navbar/>
+      <Routes>
+        <Route path='/' element={<Card/>}/>
+        <Route path="/cards" element={<Card/>} />
+        <Route path="/newcard" element={<NewCard/>} />
+      </Routes>
+     </Router>
+    </Provider>
   );
 }
 
