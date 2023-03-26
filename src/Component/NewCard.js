5924050ffef0bc593bec38f95e32dde59b3657a6
muskan2532150 <rg2532150@gmail.com>
2023-03-26 19:23:26 +0530

    Update inputs to controlled inputs

--- src/Component/NewCard.js
+++ src/Component/NewCard.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React,{ useState } from "react"
 
 const NewCard = () => {
+    const [inputs, setInputs] = useState({email:'', password:'', video:''});
+
+   const inputHandler = (e) => {
+   console.log(e.target.name, e.target.value)
+   setInputs({
+    ...inputs,
+    [e.target.name] : e.target.value
+})
+console.log(inputs);
+   }
+  
     return (
         <form className='new-form'>
             <div className="mb-3">
-                <input type="email" className="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email address' />
+                <input type="email" name="email" className="form-control form-control-lg" onChange={inputHandler} 
+                value={inputs.email} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email address' />
                 <div id="emailHelp" className="form-text text-white">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
-                <input type="password" className="form-control form-control-lg" id="exampleInputPassword1" placeholder='Password' />
+                <input type="password" name="password" className="form-control form-control-lg" id="exampleInputPassword1" 
+                onChange={inputHandler} placeholder='Password' value={inputs.password} />
             </div>
-            <select className="form-select-lg mb-3" aria-label="Default select example">
+            <select className="form-select-lg mb-3" aria-label="Default select example"
+            name="video" onChange={inputHandler} value={inputs.video}>
                 <option defaultValue={"Entertainment Videos"}>Open this select menu</option>
                 <option value="Education Videos">Education Videos</option>
                 <option value="Finance Videos">Finance Videos</option>
