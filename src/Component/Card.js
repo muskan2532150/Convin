36bdd628c04bc283bb02e5af37611e50ca4a0139
muskan2532150 <rg2532150@gmail.com>
2023-03-26 17:20:12 +0530

    Create a card component

---
+++ src/Component/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                <a href="#" className="card-link">Card link</a>
+            </div>
+        </div>
+    )
+}
+
+export default Card
