74d29d92bcaf9f5dad02f96d12253ce79f8ee8cf
muskan2532150 <rg2532150@gmail.com>
2023-03-26 18:18:25 +0530

    Update css for card form

--- src/Component/NewCard.js
+++ src/Component/NewCard.js
@@ -4,15 +4,13 @@ const NewCard = () => {
     return (
         <form className='new-form'>
             <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <input type="email" className="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email address' />
+                <div id="emailHelp" className="form-text text-white">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+                <input type="password" className="form-control form-control-lg" id="exampleInputPassword1" placeholder='Password' />
             </div>
-            <select className="form-select mb-3" aria-label="Default select example">
+            <select className="form-select-lg mb-3" aria-label="Default select example">
                 <option defaultValue={"Entertainment Videos"}>Open this select menu</option>
                 <option value="Education Videos">Education Videos</option>
                 <option value="Finance Videos">Finance Videos</option>
