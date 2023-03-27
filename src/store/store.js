4a1ad90c255bd5eb4b044824c591000861c7ca31
muskan2532150 <rg2532150@gmail.com>
2023-03-27 17:41:43 +0530

    Update store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import CardSliceReducer from './cardSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    card:CardSliceReducer
+  },
 })
