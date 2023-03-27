39717bb75e0a5b00b8ee8b9ab4e013a128f77315
muskan2532150 <rg2532150@gmail.com>
2023-03-27 23:01:39 +0530

    Add modal for each card

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -21,7 +21,21 @@ const Card = () => {
               {card.name}
             </h5>
             <h6 className="card-subtitle mb-2 text-body-secondary">{card.video}</h6>
-            <Link to={card.url} className="card-link">Card link</Link>
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" 
+            data-bs-target={`#${card.id}`}>See More </button>
+              <div className="modal fade" id={card.id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h1 className="modal-title fs-5 text-dark" id="exampleModalLabel">{card.name}</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                    <iframe src={card.url} allow="autoplay" ></iframe>
+                    </div> 
+                </div>
+            </div>
+        </div>
             <button type="button" className="btn btn-sm btn-danger ms-3" onClick={() => deleteHandler(card)}>Delete</button>
           </div>
         </div>
