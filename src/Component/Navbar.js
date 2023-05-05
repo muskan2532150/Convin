576dfb5df767192f785332f5a415661aa4d4bb50
muskan2532150 <rg2532150@gmail.com>
2023-05-05 13:28:14 +0530

    Update  Navbar with the figma design

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,12 +1,9 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setdata } from '../store/cardSlice';
+import '../assets/Navbar.css';
 
 const Navbar = () => {
-  const cardstate = useSelector((state) => state.card);
   const [query, setQuery] = useState('');
-  const dispatch = useDispatch();
 
   const SearchInput = (e) => {
     setQuery(e.target.value);
@@ -14,31 +11,23 @@ const Navbar = () => {
 
   const searchHandler = (e) => {
     e.preventDefault();
-    const cards = cardstate.filter((item) => item.video === query);
-    dispatch(setdata(cards));
   };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary custom-nav">
       <div className="container-fluid">
-        <Link className="navbar-brand fs-2" to="/">Convin</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
+        <div>
+          <Link className="navbar-brand fs-2" to="/">LOGO</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+        </div>
         <div className="collapse navbar-collapse fs-4" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/cards">List</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/newcard">New Card</Link>
-            </li>
-          </ul>
           <form className="d-flex" role="search">
             <input
-              className="form-control me-2"
+              className="form-control me-2 srinput"
               type="search"
-              placeholder="Search"
+              placeholder="Search for your favorite groups in ATG"
               aria-label="Search"
               value={query}
               onChange={SearchInput}
@@ -46,6 +35,10 @@ const Navbar = () => {
             <button className="btn btn-outline-success" type="submit" onClick={searchHandler}>Search</button>
           </form>
         </div>
+        <button type="button" className="btn">
+          create account.
+          <span>It's free!</span>
+        </button>
       </div>
     </nav>
   );
