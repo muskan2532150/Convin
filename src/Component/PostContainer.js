d33b792d9c74bd68d026d40cb29a39dc6c42b882
muskan2532150 <rg2532150@gmail.com>
2023-05-09 01:55:35 +0530

    Add breakpoint css for li

--- src/Component/PostContainer.js
+++ src/Component/PostContainer.js
@@ -12,7 +12,7 @@ const PostContainer = () => {
       updatePosts(fetchPosts);
     }
   }, [fetchPosts]);
-  console.log(posts);
+
   const LinkHandler = (typeData) => {
     if (typeData === 'All Posts') {
       updatePosts([...fetchPosts]);
@@ -37,25 +37,25 @@ const PostContainer = () => {
         <div className="d-flex justify-content-between pe-3 align-items-center">
           <ul className="d-flex list-unstyled">
             <li>
-              <button className="btn" onClick={() => LinkHandler('All Posts')} type="button">All Posts</button>
+              <button className="btn li-btn" onClick={() => LinkHandler('All Posts')} type="button">All Posts</button>
             </li>
             <li>
               {' '}
-              <button className="btn" onClick={() => { LinkHandler('Article'); }} type="button">Article</button>
+              <button className="btn li-btn" onClick={() => { LinkHandler('Article'); }} type="button">Article</button>
             </li>
             <li>
-              <button className="btn" onClick={() => LinkHandler('Meetup')} type="button">Event</button>
-            </li>
-            <li>
-              {' '}
-              <button className="btn" onClick={() => LinkHandler('Education')} type="button">Education</button>
+              <button className="btn li-btn" onClick={() => LinkHandler('Meetup')} type="button">Event</button>
             </li>
             <li>
               {' '}
-              <button className="btn" onClick={() => LinkHandler('Job')} type="button">Job</button>
+              <button className="btn li-btn" onClick={() => LinkHandler('Education')} type="button">Education</button>
+            </li>
+            <li>
+              {' '}
+              <button className="btn li-btn" onClick={() => LinkHandler('Job')} type="button">Job</button>
             </li>
           </ul>
-          <div className="d-flex gap-3">
+          <div className="d-none d-sm-flex gap-3">
             <button className="bdcolor btn" type="button">Write a Post</button>
             <button className="btn bordrx btn bordercolor" type="button">Join Group</button>
           </div>
