11df9c5292264f4064b3cc6da09b4e53e5906230
muskan2532150 <rg2532150@gmail.com>
2023-05-22 12:35:35 +0530

    Update Postform css

--- src/Component/Postform.js
+++ src/Component/Postform.js
@@ -35,140 +35,135 @@ const Postform = () => {
   };
 
   return (
-    <section className="postform">
-      <h3>Post Form</h3>
-      <form className="sign-form d-flex flex-column">
-        <div className="input-group input-group-lg">
-          <input
-            type="text"
-            placeholder="Author Name"
-            name="author"
-            className="form-control mb-sm-3"
-            id="author"
-            value={state.author}
-            onChange={handleChange}
-            required
-          />
-        </div>
+    <section className="container postform custom-background">
+      <div className="form-container">
+        <form className="sign-form d-flex flex-column">
+          <h3 className="mb-4 text-center">Post Form</h3>
+          <div className="mb-3 ">
+            <input
+              type="text"
+              placeholder="Author Name"
+              name="author"
+              className="form-control mb-sm-3 custom-font"
+              id="author"
+              value={state.author}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="input-group input-group-lg">
-          <input
-            type="text"
-            name="author_img"
-            placeholder="enter author image url"
-            className="form-control mb-sm-3"
-            id="author_img"
-            value={state.author_img}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="mb-3 ">
+            <input
+              type="text"
+              name="author_img"
+              placeholder="enter author image url"
+              className="form-control mb-sm-3 custom-font"
+              id="author_img"
+              value={state.author_img}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="input-group input-group-lg">
-          <input
-            type="text"
-            name="title_img"
-            placeholder="enter title image url"
-            className="form-control mb-sm-3"
-            id="title_img"
-            value={state.title_img}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="title_img"
+              placeholder="enter title image url"
+              className="form-control mb-sm-3 custom-font"
+              id="title_img"
+              value={state.title_img}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="input-group input-group-lg">
-          <input
-            type="text"
-            name="desc"
-            placeholder="descrpition"
-            className="form-control mb-sm-3"
-            id="desc"
-            value={state.desc}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="desc"
+              placeholder="descrpition"
+              className="form-control mb-sm-3 custom-font"
+              id="desc"
+              value={state.desc}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="input-group input-group-lg">
-          <input
-            type="text"
-            name="url"
-            placeholder="enter post url"
-            className="form-control mb-sm-3"
-            id="url"
-            value={state.url}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="url"
+              placeholder="enter post url"
+              className="form-control mb-sm-3 custom-font"
+              id="url"
+              value={state.url}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="input-group input-group-lg">
-          <input
-            type="text"
-            name="title"
-            placeholder="title"
-            value={state.title}
-            className="form-control mb-sm-3"
-            id="title"
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="title"
+              placeholder="title"
+              value={state.title}
+              className="form-control mb-sm-3 custom-font"
+              id="title"
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="input-group input-group-lg">
-          <input
-            type="date"
-            name="date"
-            placeholder="date"
-            value={state.date}
-            className="form-control mb-sm-3"
-            id="date"
-            onChange={handleChange}
-          />
-        </div>
+          <div className="mb-3">
+            <input
+              type="date"
+              name="date"
+              placeholder="date"
+              value={state.date}
+              className="form-control mb-sm-3 custom-font"
+              id="date"
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className="input-group input-group-lg">
-          <input
-            placeholder="text"
-            type="type"
-            name="type"
-            className="form-control mb-sm-3"
-            id="type"
-            value={state.type}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <input
+              placeholder="text"
+              type="type"
+              name="type"
+              className="form-control mb-sm-3 custom-font"
+              id="type"
+              value={state.type}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        {/* <select class="form-select" id="floatingSelect" a
-        ria-label="Floating label select example">
-          <option selected>Open this select menu</option>
-          <option value="1">One</option>
-        </select> */}
+          <div className="mb-3">
+            <input
+              type="location"
+              placeholder="location"
+              name="location"
+              className="form-control mb-sm-3 custom-font"
+              id="location"
+              value={state.location}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className="input-group input-group-lg">
-          <input
-            type="location"
-            placeholder="location"
-            name="location"
-            className="form-control mb-sm-3"
-            id="location"
-            value={state.location}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button
-          type="submit"
-          onClick={handlelogin}
-          className="btn custom-sign-btn text-white my-2 form-btn"
-        >
-          {' '}
-          Create Post
-        </button>
-      </form>
-
+          <button
+            type="submit"
+            onClick={handlelogin}
+            className="btn btn-secondary text-white my-2 p-2 custom-font"
+          >
+            {' '}
+            Create Post
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
