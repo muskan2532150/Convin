b5c6b9fd89978e62aeb14dd7c2b24f4f57a9a0df
muskan2532150 <rg2532150@gmail.com>
2023-06-01 01:05:35 +0530

    Add user Profile model test cases

--- server/spec/models/user_profile_spec.rb
+++ server/spec/models/user_profile_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UserProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @first_userprofile = UserProfile.new(headline: 'headline1', summary: 'egxdmikebenson', education: 'school1')
+  end
+
+  context 'UserProfile model validations' do
+    it 'is valid with valid attributes' do
+      expect(@first_userprofile).to be_valid
+    end
+
+    it 'is not valid for headline length greater than 80' do
+      @first_userprofile.headline = 'fgjhhdgjv fgesj hghghhhffhcgfcgghfhgfhgfjjjhhjdg vfyd gjcgsxxdg h sggsx gd cvkfhcjv xdjxhfhgghfhghgfgjfjf'
+      expect(@first_userprofile).to_not be_valid
+    end
+  end
 end
