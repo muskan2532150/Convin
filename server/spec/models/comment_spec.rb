738838f2d71daf8543234de023a4e47bf533691a
muskan2532150 <rg2532150@gmail.com>
2023-06-02 01:05:35 +0530

    Add comment model test cases

--- server/spec/models/comment_spec.rb
+++ server/spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @first_comment = Comment.new(content: 'mikbensondcndjncvk')
+  end
+
+  context 'Comment model validations' do
+    it 'is valid with valid attributes' do
+      expect(@first_comment).to be_valid
+    end
+
+    it 'is not valid without a content' do
+      @first_comment.content = nil
+      expect(@first_comment).to_not be_valid
+    end
+  end
 end
