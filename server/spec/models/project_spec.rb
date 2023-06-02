b2e6b6e0b02d6e418388a1ef4b9b4b6756bf1bc3
muskan2532150 <rg2532150@gmail.com>
2023-06-03 01:05:35 +0530

    Add project model test cases

--- server/spec/models/project_spec.rb
+++ server/spec/models/project_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @first_project = Project.new(name: 'Project1', des: "first_project", url:"gft.com",img:"hjkhjj.png")
+  end
+
+  context 'Project model validations' do
+    it 'is valid with valid attributes' do
+      expect(@first_project).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      @first_project.name = nil
+      expect(@first_project).to_not be_valid
+    end
+
+    it 'is not valid without a des' do
+      @first_project.des = nil
+      expect(@first_project).to_not be_valid
+    end
+
+    it 'is not valid without a url' do
+      @first_project.url = nil
+      expect(@first_project).to_not be_valid
+    end
+  end
 end
