5c77ff47ec4fe36bd8dcac59f0ec7c89e11e0f46
muskan2532150 <rg2532150@gmail.com>
2023-06-08 01:40:35 +0530

    Update user controller add `index`,`show`,`create`,`update`,`destroy` action

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+    attr_reader :user_params,:user_find
+    before_action :user_find, only: [:show,:update,:destroy]
+    def index
+        @user = User.all
+        render json: @user
+    end
+
+    def show
+        render json: @user
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: {message: "Record created successfully", status: :ok }
+        else
+            render json: {error: @user.errors.full_message}
+        end
+    end
+
+    def destroy
+        if @user.destroy
+            render json: {message: "Record destroy successfully", status: :ok }
+        else
+            render json: {error: @user.errors.full_message}
+        end 
+    end
+
+    def update
+        if @user.update(user_params)
+            render json: {message: "Record updated successfully", status: :ok }
+        else
+            render json: {error: @user.errors.full_message}
+        end
+    end
+
+    private
+
+    def user_find
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name,:email,:password)
+    end
 end
