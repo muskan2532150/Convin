a8653a2f1eeb186a59d202863f9d81c9c6ba5bc3
muskan2532150 <rg2532150@gmail.com>
2023-06-08 12:40:35 +0530

    Generate project controller and add actions

--- server/app/controllers/projects_controller.rb
+++ server/app/controllers/projects_controller.rb
@@ -1,2 +1,48 @@
 class ProjectsController < ApplicationController
+    attr_reader :project_params,:project_find
+    before_action :project_find, only: [:show,:update,:destroy]
+
+    def index
+        @project = Project.all
+        render json: @project
+    end
+
+    def show
+        render json: @project
+    end
+
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+            render json: {message: "Record created successfully", status: :ok }
+        else
+            render json: {error: @project.errors.full_message}
+        end
+    end
+
+    def destroy
+        if @project.destroy
+            render json: {message: "Record destroy successfully", status: :ok }
+        else
+            render json: {error: @project.errors.full_message}
+        end 
+    end
+
+    def update
+        if @project.update(project_params)
+            render json: {message: "Record updated successfully", status: :ok }
+        else
+            render json: {error: @project.errors.full_message}
+        end
+    end
+
+    private
+
+    def project_find
+        @project = Project.find(params[:id])
+    end
+
+    def project_params
+        params.require(:project).permit(:name,:url,:des,:img)
+    end
 end
