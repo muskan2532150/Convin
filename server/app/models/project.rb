25cfe22355feae6ddba48b0614d976ecb17aa014
muskan2532150 <rg2532150@gmail.com>
2023-06-12 12:40:35 +0530

    Add association in post model

--- server/app/models/project.rb
+++ server/app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
     validates :name, presence: true
     validates :des, presence: true, length: {maximum: 70}
     validates :url, presence: true
+
+    has_and_belongs_to_many :skills
 end
